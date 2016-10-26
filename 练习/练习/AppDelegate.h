24a106df5099c87929183eb3f23fb5581ec2770e
tarena <787116@163.com>
2016-10-26 21:16:25 +0800

    a

---
+++ 练习/练习/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  练习
+//
+//  Created by tarena on 16/10/26.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 练习/练习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  练习
+//
+//  Created by tarena on 16/10/26.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
